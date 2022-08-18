dd82b50f39db4825a0b9f8ab0b5d73a43c1a09ab
Ito Tatsuaki <tatsuaki.ito@MOB22049.local>
2022-08-18 09:50:42 +0900

    [FEAT] Add rspec configration

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Feedeli
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       controller_specs: false,
+                       routing_specs: false
+    end
   end
 end
